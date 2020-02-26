436644dd5b9a07918261bf3b3b85e98dccc46b6b
Christine <christine-xia@hotmail.com>
2020-02-26 13:47:59 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-// CONTINUOUS LOOPING PAGE https://stackoverflow.com/questions/16993693/continuous-looping-page-not-infinite-scroll
-
-// $('document').ready(function() {
-// 	$(document).scroll(function(){
-// 		if(document.documentElement.clientHeight + 
-// 			$(document).scrollTop() >= document.body.offsetHeight 
-// 		)$(document).scrollTop(0);
-//     });
-// }); 
-
 // SPLIT ARRAY https://www.geeksforgeeks.org/javascript-string-prototype-split-function/
 // SPLIT TEXT BUT KEEP DELIMITERS https://stackoverflow.com/questions/2206378/how-to-split-a-string-but-also-keep-the-delimiters
 
@@ -31,21 +21,14 @@ $('button').click(function(){
 
     $(this).css({'opacity':'0','height':'0'});
     $('button img').css({'height':'0'});
-    $('*').css({'cursor':'none'});
-    $('p').css({'cursor':'none'});
-    $('a').css({'cursor':'none'})
-
 
     // ANIMATED SCROLL https://github.com/aamirafridi/jQuery.Marquee
     // https://stackoverflow.com/questions/16475198/jquery-scrolltop-animation
-
-    var body = $("html, body");
 
     setTimeout(function(){
 
         $('.wrapper').marquee({
             speed: 1,
-            // delayBeforeStart: 0, // not working...
             direction: 'up',
             duplicated: true,
             startVisible: true
@@ -82,19 +65,12 @@ $('button').click(function(){
                 text = data;
             }
         });
-        // console.log(text)
         return text;
 
     })(); 
 
-    // console.log('Image Data:', img[0].url)
-
-    // console.log(text[2]);
-
 
     // APPEND IMAGES AND TEXT
-
-    // $('.wrapper').append('<div class="text-container"><p src="' + text + '"></p></div>');
 
     for (var i = 0; i < 500; i++) {
 
